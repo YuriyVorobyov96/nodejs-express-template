43f1815552d451e367720078c13c92cd4db4f964
YuriyVorobyov96 <darthrevan1333@gmail.com>
2023-09-01 12:38:23 +0300

    example: users module

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,5 +1,7 @@
-import { injectable } from 'inversify';
+import { inject, injectable } from 'inversify';
 
+import TYPES from '../../common/dependency-injection/types';
+import { IConfigService } from '../../config/interfaces/config.service.interface';
 import User from './entities/user.entity';
 import { IUserLogin } from './interfaces/user-login.interface';
 import { IUserRegister } from './interfaces/user-register.interface';
@@ -7,13 +9,19 @@ import { IUsersService } from './interfaces/users.service.interface';
 
 @injectable()
 export default class UsersService implements IUsersService {
+  constructor(
+    @inject(TYPES.ConfigService) private configService: IConfigService,
+  ) {}
+
   public async create({
     email,
     password,
   }: IUserRegister): Promise<User | null> {
     const user = new User(email);
 
-    await user.setPassword(password, 10);
+    const saltRound = this.configService.get('SALT');
+
+    await user.setPassword(password, Number(saltRound));
 
     return user;
   }

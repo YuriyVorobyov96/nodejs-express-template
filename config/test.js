b7fa75453fb9a2342bf80320d4199d2870f362b1
YuriyVorobyov96 <darthrevan1333@gmail.com>
2023-09-01 12:38:25 +0300

    added jest

--- config/test.js
+++ config/test.js
@@ -2,20 +2,10 @@ module.exports = {
   app: {
     port: '3000',
   },
-  typeorm: {
-    type: 'postgres',
-    host: 'localhost',
-    port: 5432,
-    username: 'a2seven',
-    password: 'q1q1',
-    database: 'a2seven',
-    synchronize: false,
-    logging: false,
-  },
   jwt: {
     secret: 'test',
   },
   password: {
-    salt_round: 3,
+    salt_round: 1,
   },
 };
